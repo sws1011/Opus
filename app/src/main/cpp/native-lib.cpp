3f03429fcae51e348493a38159b5f8b260da5750
sws1011 <397141365@qq.com>
2019-05-17 18:43:16 +0800

    update

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,108 @@
+#include <jni.h>
+#include <string>
+#include <opus.h>
+#include "_android_log_print.h"
+
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_sws_opus_OpusUtil_createEncoder(JNIEnv *env, jclass type, jint sampleRateHz, jint channel,
+                                            jint complexity) {
+    int error;
+    OpusEncoder *opusEncoder = opus_encoder_create(sampleRateHz, channel, OPUS_APPLICATION_RESTRICTED_LOWDELAY, &error);
+
+    if (error == OPUS_OK) {
+        opus_encoder_ctl(opusEncoder, OPUS_SET_VBR(0));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_VBR_CONSTRAINT(true));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_COMPLEXITY(complexity));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_SIGNAL(OPUS_SIGNAL_VOICE));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_LSB_DEPTH(16));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_DTX(0));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_INBAND_FEC(0));
+        opus_encoder_ctl(opusEncoder, OPUS_SET_PACKET_LOSS_PERC(0));
+        return reinterpret_cast<jlong>(opusEncoder);
+    }
+    return -1;
+}
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_sws_opus_OpusUtil_createDecoder(JNIEnv *env, jclass type, jint sampleRateHz, jint channel) {
+
+    int error;
+
+    OpusDecoder *opusDecoder = opus_decoder_create(sampleRateHz, channel, &error);
+
+    if (OPUS_OK == error) {
+        return reinterpret_cast<jlong>(opusDecoder);
+    }
+    return -1;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_sws_opus_OpusUtil_destroyEncoder(JNIEnv *env, jclass type, jlong handle) {
+    OpusEncoder *opusEncoder = (OpusEncoder *) handle;
+    if (!opusEncoder) {
+        return;
+    }
+    opus_encoder_destroy(opusEncoder);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_sws_opus_OpusUtil_destroyDecoder(JNIEnv *env, jclass type, jlong handle) {
+    OpusDecoder *opusDecoder = (OpusDecoder *) handle;
+    if (!opusDecoder) {
+        return;
+    }
+    opus_decoder_destroy(opusDecoder);
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_sws_opus_OpusUtil_encode(JNIEnv *env, jclass type, jlong handle, jshortArray in_, jint offset,
+                                     jbyteArray out_) {
+    jshort *in = env->GetShortArrayElements(in_, NULL);
+    jsize inLen = env->GetArrayLength(in_);
+
+    jbyte *out = env->GetByteArrayElements(out_, NULL);
+    jsize outLen = env->GetArrayLength(out_);
+
+    OpusEncoder *opusEncoder = (OpusEncoder *) handle;
+
+    if (opusEncoder <= 0 || !out || !in) {
+        LOGD("初始化数据错误---------");
+        return -1;
+    }
+
+    int encode = opus_encode(opusEncoder, in, inLen, reinterpret_cast<unsigned char *>(out), outLen);
+
+    env->ReleaseShortArrayElements(in_, in, 0);
+    env->ReleaseByteArrayElements(out_, out, 0);
+
+    return encode;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_sws_opus_OpusUtil_decode(JNIEnv *env, jclass type, jlong handle, jbyteArray encode_, jshortArray out_) {
+    jbyte *encode = env->GetByteArrayElements(encode_, NULL);
+    jshort *out = env->GetShortArrayElements(out_, NULL);
+
+    jsize encodeLen = env->GetArrayLength(encode_);
+    jsize outLen = env->GetArrayLength(out_);
+
+    OpusDecoder *opusDecoder = (OpusDecoder *) handle;
+    if (opusDecoder <= 0 || !encode || !out)
+        return -1;
+    if (encodeLen <= 0 || outLen <= 0) {
+        return -1;
+    }
+
+    int decode = opus_decode(opusDecoder, reinterpret_cast<const unsigned char *>(encode), encodeLen, out, outLen, 0);
+
+    env->ReleaseByteArrayElements(encode_, encode, 0);
+    env->ReleaseShortArrayElements(out_, out, 0);
+    return decode;
+}
---
+++ app/src/main/java/com/sws/opus/MainActivity.java
@@ -0,0 +1,282 @@
+package com.sws.opus;
+
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 原始音频文件路径
+     */
+    private static final String AUDIO_FILE_PATH = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio.pcm";
+
+    /**
+     * 处理过的音频文件路径
+     */
+    private static final String AUDIO_PROCESS_FILE_PATH = Environment.getExternalStorageDirectory().getPath() +
+            "/opus_process.pcm";
+
+    private long mEncoder;
+    private long mDecoder;
+
+    private AudioTrack mAudioTrack;
+    private int mMinBufferSize;
+    private File mFile;
+    private File mProcessFile;
+    private boolean isInitialized;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initAudioRecord();
+        if (VERSION.SDK_INT >= VERSION_CODES.M && PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(getApplicationContext(), permission.WRITE_EXTERNAL_STORAGE)) {
+            requestPermissions(new String[]{permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        } else {
+            initAudio();
+        }
+        setup();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initAudio();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setup() {
+        Button playingBtn = findViewById(R.id.playing);
+        Button playingAgcNsBtn = findViewById(R.id.playing_process);
+        Button agcNsProcessBtn = findViewById(R.id.opus_btn);
+
+        playingBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isInitialized || !mFile.exists()) {
+                    Toast.makeText(MainActivity.this, "文件读写失败", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                playing(false);
+            }
+        });
+
+        playingAgcNsBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isInitialized && !mProcessFile.exists() || mProcessFile.length() <= 0) {
+                    Toast.makeText(MainActivity.this, "文件读写失败", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                playing(true);
+            }
+        });
+
+        agcNsProcessBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isInitialized || !mFile.exists()) {
+                    Toast.makeText(MainActivity.this, "文件读写失败", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                process();
+            }
+        });
+    }
+
+    private void initAudio() {
+        mProcessFile = new File(AUDIO_PROCESS_FILE_PATH);
+
+        mFile = new File(AUDIO_FILE_PATH);
+
+        if (!mFile.exists() || mFile.length() <= 0) {
+            Log.e("sws", " init file-----------");
+            AssetManager assets = getAssets();
+            try {
+                InputStream inputStream = assets.open("record/recorded_audio.pcm");
+                FileOutputStream fileOutputStream = new FileOutputStream(mFile);
+                byte[] buf = new byte[1024 * 1024];
+                int len;
+                while ((len = inputStream.read(buf)) != -1) {
+                    fileOutputStream.write(buf, 0, len);
+                }
+                inputStream.close();
+                fileOutputStream.close();
+                isInitialized = true;
+                Log.e("sws", " init file end-----------");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("sws", "-----------");
+            isInitialized = true;
+        }
+    }
+
+    private void initAudioRecord() {
+        if (mAudioTrack == null) {
+            mMinBufferSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+            mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, mMinBufferSize, AudioTrack.MODE_STREAM);
+        }
+    }
+
+    private void process() {
+        if (mEncoder == 0) {
+            //压缩质量 complexity 4-20 3-28
+            //1 单声道
+            mEncoder = OpusUtil.createEncoder(8000, 1, 4);
+        }
+
+        Log.e("sws", "encode ===" + mEncoder);
+        Log.e("sws", "decoder ===" + mDecoder);
+        try {
+            FileInputStream ins = new FileInputStream(mFile);
+            File outFile = new File(AUDIO_PROCESS_FILE_PATH);
+            FileOutputStream out = new FileOutputStream(outFile);
+//            码率 V = 8K * 16 * 1 = 128Kbps = 16KBps
+//            音频帧时间为20ms，每帧音频数据大小为 size = 16KBps * 0.02s = 320KB，即160 Short，
+            byte[] buf = new byte[320];
+
+            while (ins.read(buf) != -1) {
+                short[] shortData = new short[buf.length >> 1];
+
+                byte[] processData = new byte[20];
+
+                ByteBuffer.wrap(buf).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shortData);
+
+                int encode = OpusUtil.encode(mEncoder, shortData, 0, processData);
+                Log.e("sws", "压缩后的大小======" + encode);
+                if (encode > 0) {
+                    out.write(processData);
+                }
+            }
+            Toast.makeText(getApplicationContext(), "完成", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.e("sws", "opus end======");
+    }
+
+    private boolean isPlaying;
+
+    private void playing(final boolean isPlayingProcess) {
+        if (isPlaying) {
+            isPlaying = false;
+            mAudioTrack.stop();
+        }
+        if (mDecoder == 0) {
+            mDecoder = OpusUtil.createDecoder(8000, 1);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                InputStream ins = null;
+                try {
+                    File file = mFile;
+                    if (isPlayingProcess) {
+                        file = mProcessFile;
+                    }
+
+                    isPlaying = true;
+
+                    ins = new FileInputStream(file);
+                    mAudioTrack.play();
+                    byte[] buf;
+                    short[] decodeBuf = null;
+                    if(isPlayingProcess) {
+                        buf = new byte[20];
+                        decodeBuf = new short[160];
+                    } else {
+                        buf = new byte[mMinBufferSize];
+                    }
+                    int len;
+
+                    while ((len = ins.read(buf)) != -1 && mAudioTrack != null && isPlaying) {
+                        if (isPlayingProcess) {
+                            int decode = OpusUtil.decode(mDecoder, buf, decodeBuf);
+                            Log.e("sws", "解压后的大小==" + decode);
+                            mAudioTrack.write(decodeBuf, 0, decode);
+                        } else {
+                            mAudioTrack.write(buf, 0, len);
+                        }
+                    }
+                    if (mAudioTrack == null) {
+                        return;
+                    }
+                    mAudioTrack.stop();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (ins != null) {
+                        try {
+                            ins.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private byte[] shortsToBytes(short[] data) {
+        byte[] buffer = new byte[data.length * 2];
+        int shortIndex, byteIndex;
+        shortIndex = byteIndex = 0;
+        for (; shortIndex != data.length; ) {
+            buffer[byteIndex] = (byte) (data[shortIndex] & 0x00FF);
+            buffer[byteIndex + 1] = (byte) ((data[shortIndex] & 0xFF00) >> 8);
+            ++shortIndex;
+            byteIndex += 2;
+        }
+        return buffer;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isPlaying = false;
+        if (mAudioTrack != null) {
+            mAudioTrack.release();
+            mAudioTrack = null;
+        }
+        if (mEncoder > 0) {
+            OpusUtil.destroyEncoder(mEncoder);
+        }
+        if (mDecoder > 0) {
+            OpusUtil.destroyDecoder(mDecoder);
+        }
+    }
+}
